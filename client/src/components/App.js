b5222e8fd20f835fca936da2a26a2ca5a85a1a44
Matt Trim <contact@matttrim.com>
2022-01-25 21:42:31 +0000

    Implement basic API authorisation

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import PasswordScreen from "../pages/PasswordScreen";
 import GenerateNewShortUrlScreen from "../pages/GenerateNewShortUrlScreen";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [isAuthed, setIsAuthed] = useState(false);
 
   return (
-    <div>
+    <div className="container">
       <h1>shorter.</h1>
         {isAuthed ? <GenerateNewShortUrlScreen /> : <PasswordScreen setIsAuthed={setIsAuthed} />}
       </div>
